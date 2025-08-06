d61bfdd22b80296053f059fda76e0b724e4896b4
lancely <842184960@qq.com>
2025-08-06 17:19:02 +0800

    优化网络请求的代理逻辑

--- src/http.ts
+++ src/http.ts
@@ -2,6 +2,8 @@ import axios from 'axios'
 import G from './global'
 import Config from './config'
 import vscode from 'vscode'
+import path from 'node:path'
+import fs from 'node:fs'
 
 const http = axios.create({
   baseURL: 'https://www.v2ex.com',
@@ -16,31 +18,33 @@ const http = axios.create({
 
 http.interceptors.request.use(config => {
   const reqUrl = new URL(config.url || '', config.baseURL)
-  // v2ex以外的请求不做额外处理
-  if (!reqUrl.host.endsWith('v2ex.com')) {
-    return config
-  }
+  const hosts = getGfwHostList()
 
-  // 添加cookie
-  if (config.headers['Cookie'] === undefined) {
-    config.headers['Cookie'] = G.getCookie() || ''
+  // 添加v2ex的cookie
+  if (reqUrl.host.endsWith('v2ex.com')) {
+    if (config.headers['Cookie'] === undefined) {
+      config.headers['Cookie'] = G.getCookie() || ''
+    }
   }
 
   // 设置代理
-  const proxyUrl = Config.proxyUrl()
-  if (proxyUrl) {
-    const isValid = checkProxyUrl(proxyUrl)
-    if (isValid) {
-      const url = new URL(proxyUrl)
-      config.proxy = {
-        protocol: url.protocol.replace(':', ''),
-        host: url.hostname,
-        port: Number(url.port)
+  if (hosts.some(host => reqUrl.host.endsWith(host))) {
+    const proxyUrl = Config.proxyUrl()
+    if (proxyUrl) {
+      const isValid = checkProxyUrl(proxyUrl)
+      if (isValid) {
+        const url = new URL(proxyUrl)
+        config.proxy = {
+          protocol: url.protocol.replace(':', ''),
+          host: url.hostname,
+          port: Number(url.port)
+        }
+      } else {
+        vscode.window.showErrorMessage('代理url格式不正确')
       }
-    } else {
-      vscode.window.showErrorMessage('代理url格式不正确')
     }
   }
+
   return config
 })
 
@@ -53,6 +57,31 @@ function checkProxyUrl(url: string) {
     }
   }
   return true
+}
+
+const _gfwList: string[] = []
+
+/** 解析gfwlist */
+function getGfwHostList() {
+  if (_gfwList.length <= 0) {
+    console.log('加载gfw列表...')
+    const fp = path.join(G.context.extensionPath, 'resources', 'gfwlist.txt')
+    let str = fs.readFileSync(fp, 'utf-8')
+    // base64解码
+    str = Buffer.from(str, 'base64').toString('utf-8')
+    // 解析gfwlist
+    const lines = str.split('\n')
+    for (const line of lines) {
+      if (line.startsWith('||')) {
+        _gfwList.push(line.substring(2))
+      }
+      if (line.startsWith('.')) {
+        _gfwList.push(line.substring(1))
+      }
+    }
+    console.log(`gfw列表加载完成，共${_gfwList.length}个域名`)
+  }
+  return _gfwList
 }
 
 export default http
