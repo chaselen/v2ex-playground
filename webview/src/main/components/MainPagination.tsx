898f7fd0ca38b060cc9ed43c49d6d3f0274ee2c2
lancely <842184960@qq.com>
2026-06-11 16:05:26 +0800

    refactor: consolidate pagination styles

--- webview/src/main/components/MainPagination.tsx
+++ webview/src/main/components/MainPagination.tsx
@@ -24,7 +24,7 @@ export default function MainPagination(props: MainPaginationProps) {
   const totalCountText = showTotalCount ? totalCount.toLocaleString('en-US') : ''
 
   return (
-    <>
+    <div className={styles['main-pagination']}>
       <Pagination
         size="small"
         pageSize={1}
@@ -37,6 +37,6 @@ export default function MainPagination(props: MainPaginationProps) {
       {showTotalCount && (
         <span className={styles['main-pagination-summary']}>{`共 ${totalCountText} 条`}</span>
       )}
-    </>
+    </div>
   )
 }
